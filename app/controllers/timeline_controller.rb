dd4193a846cbb3210a9ec480b3ae5e2bad90a476
Érico Medeiros <erico.medeiros@gmail.com>
2016-09-14 15:00:45 -0300

    Alteração de CSS, Ordenação de Posts por data e formatação de Data. Adição do símbolo de like.

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,7 +1,21 @@
 class TimelineController < ApplicationController
 	def show
 		require 'open-uri'
+		require 'date'
 		@json = open("http://vagalumewifi.com.br/timeline.json")
 		@dados = JSON.load(@json)
+		@dados.sort! { |a,b| date_to_stamp(a['date']) <=> date_to_stamp(b['date']) }.reverse!
+		@dados.each { |post| post['date'] = datetime_parse(post['date'])}
+	end
+
+	private
+	def datetime_parse(string)
+		datetime = DateTime.parse(string)
+		datetime.strftime('%d %b %Y, %H:%M')
+	end
+
+	def date_to_stamp(string)
+		datetime = DateTime.parse(string)
+		datetime.to_i
 	end
 end
